53ab5bdc60f9eb6969a3b09aa91442e20c24a43b
Peter Williams <peter.williams.97@gmail.com>
2011-11-05 22:27:00 +1100

    completed neural net exercise

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -20,8 +20,21 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+X1 = [ones(m,1), X];
+%size(X1)
+z1 = X1 * Theta1';
+%z11 = [ones(m,1), z1];
+a1 = sigmoid(z1);
+a11 = [ones(m,1), a1];
+z2 = a11 * Theta2';
+%z21 = [ones(m,1), z2 ];
+a2 = sigmoid(z2);
 
-
+%size(a2)
+%a2(1,:)
+[_,p] = max(a2, [], 2);
+%p = p - ones(m,1);
+%p(1)
 
 
 
