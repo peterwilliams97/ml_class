9b49bec68526faca9b5fbf37accce0b10935fef8
Peter Williams <peter.williams.97@gmail.com>
2011-11-15 21:59:57 +1100

    simplified back propagation code

--- mlclass-ex4/nnCostFunction.submitted.m
+++ mlclass-ex4/nnCostFunction.submitted.m
@@ -61,6 +61,12 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
+
+%X
+%y
+%Theta1
+%Theta2
+
 yb = zeros(m,num_labels);
 for i=1:m
  yb(i,y(i)) = 1;
@@ -70,16 +76,42 @@ X1 = [ones(m,1), X];
 z1 = X1 * Theta1';
 a1 = sigmoid(z1);
 a11 = [ones(m,1), a1];
-
 z2 = a11 * Theta2';
 a2 = sigmoid(z2);
 
+%[_,p] = max(a2, [], 2);
+%size(a1)
+%size(a2)
+
 % J = 1/m(-y*log(h*x) - (1-y)log(1-h*x)) + (lambda/2m)(theta1^2+ theta2^2)
-J = sum(sum( -yb.*log(a2) - (1-yb).*log(1-a2) ))/m;
+for i = 1:m
+ for k = 1:num_labels
+  p = -yb(i,k)*log(a2(i,k)) - (1-yb(i,k))*log(1-a2(i,k));
+  J = J + p;
+ end 
+end
+J = J/m;
 
+% Compute the regularization penalty
+reg_penalty = 0;
 
-% Add the regularization penalty
-J = J + (sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)) )* lambda/(2*m);
+for j = 1:size(Theta1)(1)
+ for k = 2:size(Theta1)(2)
+    reg_penalty = reg_penalty + Theta1(j,k)*Theta1(j,k);
+ end 
+end
+
+for j = 1:size(Theta2)(1)
+ for k = 2:size(Theta2)(2)
+    reg_penalty = reg_penalty + Theta2(j,k)*Theta2(j,k);
+ end 
+end
+
+reg_penalty = reg_penalty*lambda/(2*m);
+
+J = J + reg_penalty;
+
+%J = (1/m)*(-yb.*log(a2) - (1-yb).*log(1-a2));
 
 
 % -------------------------------------------------------------
@@ -100,27 +132,43 @@ for t = 1:m
     
     a_3 = sigmoid(z_3);
     
-    % Error
     yt = yb(t,:)';
     delta_3 = a_3 - yt; 
+    
+    %size(delta_3)
     
     % Propagate error backwards
     delta_2 = (Theta2' * delta_3) .* sigmoidGradient([1; z_2]);
     delta_2 = delta_2(2:end); 
+    %size(delta_2)
+ 
+  %  delta_1 = (delta_2 * Theta1);
+  %  delta_1 = delta_1; 
+  %  delta_1 = delta_1 .* sigmoidGradient(a_1);
+    %size(delta_1)
+ 
+%    delta_1 = (delta_2 * Theta1) .* sigmoidGradient(xt);
+%    delta_1 = delta_1(2:end); 
+%    size(delta_1)
+    
+    %size(a2)
+    %size(delta_3)
+    %size(Theta2_grad)
 
     dt2 = delta_3 * a_2';
+    %size(dt2)
     dt1 = delta_2 * a_1';
+    %size(dt1)
+    
  
     Theta2_grad = Theta2_grad + dt2;
     Theta1_grad = Theta1_grad + dt1;
+   
 end
 
-
 Theta1_grad = (1/m) * Theta1_grad;
-Theta2_grad = (1/m) * Theta2_grad ;
-
-% Add regularization terms
 Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + (lambda/m) * Theta1(:,2:end);
+Theta2_grad = (1/m) * Theta2_grad ;
 Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + (lambda/m) * Theta2(:,2:end);
 
 % =========================================================================
