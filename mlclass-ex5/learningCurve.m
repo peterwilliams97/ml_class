78952b2d16ee9136a0494b1189482287f33c9038
Peter Williams <peter.williams.97@gmail.com>
2011-11-19 08:18:54 +1100

    did learning curves

--- mlclass-ex5/learningCurve.m
+++ mlclass-ex5/learningCurve.m
@@ -55,11 +55,10 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
-
+for i = 1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+    [error_train(i), _] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+    [error_val(i), _] = linearRegCostFunction(Xval, yval, theta, 0);
 
 % -------------------------------------------------------------
 
